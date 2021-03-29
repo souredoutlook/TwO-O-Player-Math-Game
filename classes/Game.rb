8faf3e15e5347570a2154a8d2ee771e9f2595f2f
souredoutlook <souredoutlook@gmail.com>
2021-03-29 13:28:15 -0400

    add game_over?

--- classes/Game.rb
+++ classes/Game.rb
@@ -2,6 +2,7 @@ require_relative 'Player.rb'
 require_relative 'Question.rb'
 
 class Game
+
   def initialize(num_players)
     @players = assign_players(num_players)
     # @current_player = next_turn
@@ -26,10 +27,29 @@ class Game
   
   def play
     puts "Looks like we've got the following players: "
-    @players.each {|player| puts player.name} 
+
+    @players[0].update_lives
+
+    @players.each {|player| puts "Player # #{player.number}: #{player.name}. Has #{player.lives} lives."} 
+
+    game_over?
   end
 
   def game_over?
-  
+    updated_players = []
+    @players.each {|player|
+      if player.lives <= 0
+        puts "#{player.name} is out of lives! Player ##{player.number} is out of the game!"
+      else
+        updated_players << player
+      end
+    }
+
+    @players = updated_players;
+
+    if @players.length == 1
+      puts "Player ##{@players[0].number} is the last player standing! #{@players[0].name} is the winner!"
+      exit(0)
+    end
   end
 end
--- classes/Player.rb
+++ classes/Player.rb
@@ -1,14 +1,21 @@
 class Player
-  attr_reader :number, :name
+  attr_reader :number, :name, :lives
+
   def initialize(player_number)
     @number = player_number
     @name = name_prompt
+    @lives = 3
   end
-
+  
+  def update_lives
+  @lives -= 3
+  end
+  
   private
 
   def name_prompt
     print "What is Player #{number}'s name? "
     gets.chomp
   end
+
 end
